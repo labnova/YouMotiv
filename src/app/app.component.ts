b12849634016152f90815e9aa0901cf064c4a1bd
Labnova <progettofeb@gmail.com>
2020-06-02 12:03:35 +0200

    add material button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface PrincipalBodyData {
+  pesoComplessivo: string;
+  massaMuscolare: string;
+  massaGrassa: string;
+  bmi: string;
+  dataMisurazione: string;
+}
+
+const ELEMENT_DATA: PrincipalBodyData[] = [
+  { pesoComplessivo: "90", massaMuscolare: '48%', massaGrassa: "23.1%", bmi: 'H', dataMisurazione: '10/12/2019' },
+  { pesoComplessivo: "89", massaMuscolare: '47%', massaGrassa: "21.1%", bmi: 'H', dataMisurazione: '14/12/2019' },
+  { pesoComplessivo: "88", massaMuscolare: '44%', massaGrassa: "24.1%", bmi: 'H', dataMisurazione: '16/12/2019' },
+  { pesoComplessivo: "87", massaMuscolare: '41%', massaGrassa: "25.1%", bmi: 'H', dataMisurazione: '17/12/2019' },
+  { pesoComplessivo: "86", massaMuscolare: '47%', massaGrassa: "27.1%", bmi: 'H', dataMisurazione: '19/12/2019' },
+  { pesoComplessivo: "85", massaMuscolare: '40%', massaGrassa: "29.1%", bmi: 'H', dataMisurazione: '20/12/2019' },
+  { pesoComplessivo: "84", massaMuscolare: '44%', massaGrassa: "27.1%", bmi: 'H', dataMisurazione: '21/12/2019' },
+  { pesoComplessivo: "83", massaMuscolare: '42%', massaGrassa: "23.1%", bmi: 'H', dataMisurazione: '24/12/2019' },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
-  title = 'youmotiv';
+  title = 'YouMotiv';
+  displayedColumns = ['pesoComplessivo', 'massaMuscolare', 'bmi', 'massaGrassa', "dataMisurazione"]
+  dataForStarterTable = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
